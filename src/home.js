bfcb3e8efded3a8aa50932406a748d30fbca40f8
Jordan Reger <jordanreger@gmail.com>
2021-08-20 13:02:31 -0400

    Update home.js

--- src/home.js
+++ src/home.js
@@ -265,7 +265,7 @@ class Lander extends LitElement {
       <div id="page">
         <div id="left">
           <div id="main">
-            <video id="background" autoplay="true" controls></video>
+            <!--<video id="background" autoplay="true" controls></video>-->
           </div>
         </div>
         <div id="right">
